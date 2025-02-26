5d444574844ee9ddaab9a48d5ff9eb23fd116647
BigBinary <bigbinary@Sarthaks-Macbook-Pro.local>
2025-02-26 16:53:52 +0530

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,8 @@
 // const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
 import "../stylesheets/application.scss"
+const { initializeAxios } = require("apis/axios");
+const { initializeLogger } = require("common/logger");
+
+initializeLogger();
+initializeAxios();
---
+++ app/javascript/src/apis/axios.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { evolve } from "ramda";
+
+const setAuthHeaders = () => {
+  axios.defaults.headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "X-CSRF-TOKEN": document
+      .querySelector('[name="csrf-token"]')
+      .getAttribute("content"),
+  };
+  const token = localStorage.getItem("authToken");
+  const email = localStorage.getItem("authEmail");
+  if (token && email) {
+    axios.defaults.headers["X-Auth-Email"] = email;
+    axios.defaults.headers["X-Auth-Token"] = token;
+  }
+};
+
+const setHttpHeaders = () => {
+  axios.defaults.headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+};
+
+const transformResponseKeysToCamelCase = response => {
+  if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+const responseInterceptors = () => {
+  axios.interceptors.response.use(
+    response => {
+      transformResponseKeysToCamelCase(response);
+
+      return response.data;
+    },
+    error => Promise.reject(error)
+  );
+};
+const requestInterceptors = () => {
+  // since all ramda functions are curried by default so
+  axios.interceptors.request.use(
+    //above function is modified as below
+    evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  );
+};
+
+export const initializeAxios = () => {
+  axios.defaults.baseURL = "/";
+  setAuthHeaders();
+  setHttpHeaders();
+  requestInterceptors();
+  responseInterceptors();
+}
