1c0c1076ad4d5f7cd61a4438166e5c119fb15bca
BigBinary <bigbinary@Sarthaks-Macbook-Pro.local>
2025-03-19 15:30:40 +0530

    Corrected migration

--- db/migrate/20250225085607_create_posts.rb
+++ db/migrate/20250225085607_create_posts.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.string :title, 
+      t.string :title
       t.text :description
       t.integer :upvotes, default: 0, null: false
       t.integer :downvotes, default: 0, null: false
